31f8896189c253c0fd5551181d41e81e01cdd87a
bore-ko <tori.un.sourire@gmail.com>
2023-09-17 01:09:38 +0900

    ログイン後の画面を作成, lintの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,11 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys:[:postal_code, :address, :self_introduction])
-    devise_parameter_sanitizer.permit(:account_update, keys:[:postal_code, :address, :self_introduction])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[postal_code address self_introduction])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[postal_code address self_introduction])
   end
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(_resource)
     books_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     registrations: "users/registrations"
   }
   resources :books
+  resources :users, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
