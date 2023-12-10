ca7a6940a5329c1cf72bb607080ef852e7afe183
bore-ko <tori.un.sourire@gmail.com>
2023-12-10 18:17:06 +0900

    Railsアプリにテストを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,9 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#editable?' do
+    user = User.create!(email: 'foo@exsample.com', password: 'password')
+    report = Report.create!(user: :user, title: 'title', content: 'content')
+    assert report.editable?(user)
+  end
 end
